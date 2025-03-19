2304e440a99380081022da16a55c2cf6cf04475c
Daniel Montecillo <daniel.montecillo@montecillo.ca>
2025-03-19 15:11:50 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name="3D-bin-packing",
     version="0.1.0",
+    description="3D bin packing implementation",
     packages=find_packages(),
+    install_requires=[
+        "numpy==1.19.5",
+        "matplotlib==3.3.4",
+    ],
 )
